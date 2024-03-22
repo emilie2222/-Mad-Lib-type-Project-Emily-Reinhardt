0046f2cbf2157b27a12ed6bc1e3d49ed1ef23674
Reinhardt_Emily <reine250@hsd.k12.or.us>
2024-03-22 15:18:04 -0700

    Wrote story

---
+++ app.py
@@ -0,0 +1,26 @@
+"""The Ugly Duckling-Mad libs alternate program by Emily Reinhardt """
+
+
+"""
+Mother Duck lived on a farm. In her nest, she had (number) (x) eggs
+and one (x) egg. One day, the number (x) eggs started to crack.
+(Noise), (Noise), (Noise)! Five (positive description), (color) baby ducklings came out.
+Then the (x) egg started to crack. (Dramatic noise), (Dramatic noise), (Dramatic noise)! One big,
+ugly duckling came out. ‘(Shocked/disgusted expression)’ thought Mother Duck.
+Nobody wanted him. ‘(Mean expression)’ said his brothers and sisters. ‘You’re ugly!’
+The ugly duckling was sad. So he went to find some new friends.
+‘Go away!’ said the pig.
+‘Go away!’ said the sheep.
+‘Go away!’ said the cow.
+‘Go away!’ said the horse.
+No one wanted to be his friend. It started to get cold. It started to snow! The ugly duckling
+found an empty barn and lived there. He was cold, sad and alone.
+Then spring came. The ugly duckling left the barn and went back to the pond. He was very
+thirsty and put his beak into the water. He saw a beautiful, white bird! ‘Wow!’ he said. ‘Who’s
+that?’
+‘It’s you,’ said another beautiful, white bird.
+‘Me? But I’m an ugly duckling.’
+‘Not any more. You’re a beautiful swan, like me. Do you want to be my friend?’
+‘Yes,’ he smiled.
+All the other animals watched as the two swans flew away, friends forever.
+"""
